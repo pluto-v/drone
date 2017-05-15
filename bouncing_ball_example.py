03e02281caa4dbc1c5aeeba88aae7183c88ef4aa
burntPotatoes <tommyhuang999@gmail.com>
2017-05-15 16:27:17 -0400

    drone sprite

--- bouncing_ball_example.py
+++ bouncing_ball_example.py
@@ -13,7 +13,7 @@ ballrect = ball.get_rect()
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-        
+
     ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]
--- main.py
+++ main.py
@@ -1,17 +1,53 @@
 import pygame
+import sys
 import random
+import pyganim
+import time
 
 
 # initiate pygame
 pygame.init()
-pygame.font.init()
 
-# initialize random stuff
-displayLength = 800
-displayHeight = 400
+# initialize random stuff (e.g. colours)
+disLength = 800
+disHeight = 400
+screen = pygame.display.set_mode((disLength,disHeight))
+
+pygame.display.set_caption("drone")
 clock = pygame.time.Clock()
 
-while True:
-    pygame.display.update()  # updates display
+# colours
+sky = (70,110,220)
 
+# initialize plane
+crashed = False
+planeImg = pygame.image.load("drone.png")
+pX = 50
+pY = 50
+acc = 0
+
+while True:
+    screen.fill(sky)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    # moving plane
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_w] == 1 and acc > -3:
+        acc += - 0.25
+    elif acc < 3:
+        acc += 0.25
+    pY += acc
+
+    screen.blit(planeImg, (pX, pY))
+
+    # shooting
+
+
+    pygame.display.update()  # updates display
     clock.tick(60)  # should make it 60FPS max
+
+# exiting
+pygame.quit()
+quit()
+
