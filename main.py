608939e996d1390326bd33a17c31fc691d11e2e4
burntPotatoes <tommyhuang999@gmail.com>
2017-05-16 13:28:17 -0400

    added crashing

--- main.py
+++ main.py
@@ -40,6 +40,10 @@ maxReload = 30  # two shots per sec
 reload = 0
 curBul = 0
 
+# initialize land
+landImg = pygame.image.load("land.png")
+landX = -50
+
 
 while True:
     screen.fill(sky)
@@ -49,8 +53,18 @@ while True:
 
     # moving plane, accelerates instead of instant movement for more smoothness
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_w] == 1 and acc > -3:
+    if pressed[pygame.K_w] == 1 and acc > -3 and 10 < pY:
         acc += - 0.25
+    elif pY >= disHeight - 75:
+        # CRASH
+        # pY = disHeight - 75
+        # acc = 0
+        screen.fill((200,0,0))
+        pygame.display.update()
+        print ("YOU CRASHED")
+        time.sleep(1)
+        sys.exit()
+
     elif acc < 3:
         acc += 0.25
     pY += acc
@@ -88,9 +102,11 @@ while True:
 
             screen.blit(bulImg, (bulX[i], bulY[i]))
 
-    pygame.display.update()  # updates display
-    clock.tick(60)  # should make it 60FPS max
+    screen.blit(landImg, (landX, disHeight - 125))
 
+    pygame.display.update()  # updates display
+
+    clock.tick(60)  # should make it 60FPS max
     # exiting
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
