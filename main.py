e79fe5798dc2e9ac04db6e621fd052f104909273
burntPotatoes <tommyhuang999@gmail.com>
2017-05-15 22:33:30 -0400

    bullet shooting, doesn't quite work yet, needs fixing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import random
 import pyganim
 import time
+import math
 
 
 # initiate pygame
@@ -26,8 +27,20 @@ pX = 50
 pY = 50
 acc = 0
 
+# initialize bullets
+bulImg = pygame.image.load("bullet.png")
+bulX = -50
+bulY = -50
+bulTarX = 0
+bulTarY = 0
+bulSpeed = 20  # not sure exact thing
+reload = 30  # two shots per sec
+
 while True:
     screen.fill(sky)
+
+    # counting down timers
+    reload += -1
 
     # moving plane, accelerates instead of instant movement for more smoothness
     pressed = pygame.key.get_pressed()
@@ -42,13 +55,40 @@ while True:
 
     # shooting
     mouse = pygame.mouse.get_pressed()
-    if mouse[0] == 1:
-        #get mouse position
+    if mouse[0] == 1 and reload <= 0:
+        # get mouse position
         mousePos = pygame.mouse.get_pos()
-        mouseX = mousePos[0]
-        mouseY = mousePos[1]
+        bulTarX = mousePos[0] * 10
+        bulTarY = mousePos[1] * 10
+        # set bullet to gun position on plane sprite
+        bulX = pX + 60
+        bulY = pY + 30
+
         # test if it works
-        pygame.draw.rect(screen, (255,255,255), (mouseX - 10, mouseY - 10, 20, 20), 0)
+        # pygame.draw.rect(screen, (255,255,255), (mouseX - 10, mouseY - 10, 20, 20), 0)
+
+        reload = 30
+
+    # if bullet is in air
+    if bulTarX > 25 and bulTarY > 25:
+        # eBulSpeed = math.sqrt(abs(yDiff) ** 2 + abs(xDiff) ** 2)
+        # ratio = 350 / eBulSpeed
+        # # set bullet to certain speed
+        # yDiff *= ratio
+        # xDiff *= ratio
+        #
+        # eShotY[i] += yDiff / 100
+        # eShotX[i] += xDiff / 100
+
+        xDiff = (bulTarX - bulX)
+        yDiff = (bulTarY - bulY)
+        ratio = bulSpeed / (math.sqrt(abs(xDiff) ** 2 + abs(yDiff) ** 2))
+        xDiff *= ratio
+        yDiff *= ratio
+        bulX += xDiff
+        bulY += yDiff
+
+        screen.blit(bulImg, (bulX, bulY))
 
     pygame.display.update()  # updates display
     clock.tick(60)  # should make it 60FPS max
