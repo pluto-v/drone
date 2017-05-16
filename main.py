1b4858a338ce9f0c54a7f7f43bfcaa04baee75db
burntPotatoes <tommyhuang999@gmail.com>
2017-05-16 13:07:47 -0400

    fixed shooting

--- main.py
+++ main.py
@@ -29,14 +29,17 @@ acc = 0
 
 # initialize bullets
 bulImg = pygame.image.load("bullet.png")
-bulX = -50
-bulY = -50
-bulTarX = 0
-bulTarY = 0
-bulOrgX = 0
-bulOrgY = 0
+bulX = [-50, -50, -50]
+bulY = [-50, -50, -50]
+bulTarX = [-50, -50, -50]
+bulTarY = [-50, -50, -50]
+bulOrgX = [-50, -50, -50]
+bulOrgY = [-50, -50, -50]
 bulMaxSpeed = 20  # not sure exact thing
-reload = 30  # two shots per sec
+maxReload = 30  # two shots per sec
+reload = 0
+curBul = 0
+
 
 while True:
     screen.fill(sky)
@@ -59,28 +62,31 @@ while True:
     mouse = pygame.mouse.get_pressed()
     if mouse[0] == 1 and reload <= 0:
         # get mouse position
+        curBul += 1
+        if curBul > 2:
+            curBul = 0
         mousePos = pygame.mouse.get_pos()
-        bulTarX = mousePos[0]
-        bulTarY = mousePos[1]
-        # set bullet to gun position on plane sprite
-        bulX = pX + 60
-        bulY = pY + 30
-        bulOrgX = bulX
-        bulOrgY = bulY
+        bulTarX[curBul] = mousePos[0]
+        bulTarY[curBul] = mousePos[1]
+        bulX[curBul] = pX + 60
+        bulY[curBul] = pY + 30
+        bulOrgX[curBul] = bulX[curBul]
+        bulOrgY[curBul] = bulY[curBul]
         # test if it works
         # pygame.draw.rect(screen, (255,255,255), (mouseX - 10, mouseY - 10, 20, 20), 0)
-        reload = 30
+        reload = maxReload
 
     # if bullet is in air
-    if bulTarX > 25 and bulTarY > 25 and bulX < disLength + 100 and bulY < disHeight + 100:
-        bulSpeed = math.sqrt((bulOrgX ** 2) + (bulOrgY ** 2))
-        ratio = bulMaxSpeed / bulSpeed
-        travelX = abs(bulTarX - bulOrgX) * ratio
-        travelY = abs(bulTarY - bulOrgY) * ratio
-        bulX += travelX
-        bulY += travelY
+    for i in range(3):
+        if bulTarX[i] > -25 and bulTarY[i] > -25 and bulX[i] < disLength + 100 and bulY[i] < disHeight + 100:
+            bulSpeed = math.sqrt(((bulTarX[i] - bulOrgX[i]) ** 2) + ((bulTarY[i] - bulOrgY[i]) ** 2))
+            ratio = bulMaxSpeed / bulSpeed
+            travelX = (bulTarX[i] - bulOrgX[i]) * ratio
+            travelY = (bulTarY[i] - bulOrgY[i]) * ratio
+            bulX[i] += travelX
+            bulY[i] += travelY
 
-        screen.blit(bulImg, (bulX, bulY))
+            screen.blit(bulImg, (bulX[i], bulY[i]))
 
     pygame.display.update()  # updates display
     clock.tick(60)  # should make it 60FPS max
